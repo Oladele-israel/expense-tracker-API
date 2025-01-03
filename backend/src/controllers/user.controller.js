fa616a507aac95eab2ef7dc1d306a6576ab5274a
Oladele-israel <omoleoladele43@gmail.com>
2025-01-03 17:00:17 +0100

    added the secure tag for cookie

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -112,11 +112,11 @@ export const login = async (req, res) => {
       httpOnly: true,
       sameSite: "strict",
       maxAge: 10 * 60 * 1000,
-      secure: isProduction,
+      secure: true,
     });
     res.cookie("Sauce", refreshToken, {
       httpOnly: true,
-      secure: isProduction,
+      secure: true,
       sameSite: "strict",
       maxAge: 1 * 24 * 60 * 60 * 1000,
     });
